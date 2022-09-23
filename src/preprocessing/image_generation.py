fdd039dc26f216bb39322d61b43239af74273a3d
Atick Faisal <atickfaisal@gmail.com>
2022-09-23 04:33:50 +0300

    Generate images for training

--- src/preprocessing/image_generation.py
+++ src/preprocessing/image_generation.py
@@ -1,53 +1,116 @@
 import os
+import random
 import numpy as np
 import pyvista as pv
 
 from tqdm import tqdm
 from PIL import Image
 
+random.seed(42)
+
 DATASET_PATH = "../../data/dataset/"
+TRAIN_DIR = "Train/"
+TEST_DIR = "Test/"
 IMAGE_DIR = "Images/"
-GEOMETRY_DIR = "Geometry/"
+INPUT_DIR = "Input/"
 CURVATURE_DIR = "Curvature/"
-CFD_DIR = "Geometry_CFD_Aneurysm/"
+TARGET_DIR = "Target/"
+TRAIN_PERCENTAGE = 0.8
 
-geometries = os.listdir(os.path.join(DATASET_PATH, GEOMETRY_DIR))
-cfd_results = os.listdir(os.path.join(DATASET_PATH, CFD_DIR))
+geometries = os.listdir(os.path.join(DATASET_PATH, INPUT_DIR))
 
-for geometry in tqdm(geometries, desc="Processing ... "):
+random.shuffle(geometries)
+geometries = [filename[:-4] for filename in geometries]
+train_size = int(len(geometries) * TRAIN_PERCENTAGE)
+train_geometries = geometries[:train_size]
+test_geometries = geometries[train_size:]
+
+print("-" * 40)
+print(f"TRAIN SIZE: {len(train_geometries)}")
+print(f"TEST SIZE: {len(test_geometries)}")
+print("-" * 40)
+
+for filename in tqdm(geometries, desc="Processing ... "):
     geometry_path = os.path.join(
         DATASET_PATH,
-        GEOMETRY_DIR,
-        geometry
+        INPUT_DIR,
+        filename + ".stl"
     )
 
-    image_path = os.path.join(
+    cfd_path = os.path.join(
         DATASET_PATH,
-        IMAGE_DIR,
-        CURVATURE_DIR,
-        geometry[:-4]
+        TARGET_DIR,
+        filename + ".vtk"
     )
 
-    mesh = pv.read(geometry_path)
-    # ecap = mesh.active_scalars
-    # ecap = (ecap - np.mean(ecap)) / (np.std(ecap))
-    curvature = mesh.curvature(curv_type="mean")
-    curvature = (curvature - np.mean(curvature)) \
-        / (np.std(curvature))
+    # --------------------- GEOMETRY -----------------------
+
+    geometry = pv.read(geometry_path)
+    image_path = None
+
+    if filename in train_geometries:
+        image_path = os.path.join(
+            DATASET_PATH,
+            IMAGE_DIR,
+            TRAIN_DIR,
+            INPUT_DIR,
+            filename[:-4]
+        )
+    else:
+        image_path = os.path.join(
+            DATASET_PATH,
+            IMAGE_DIR,
+            TEST_DIR,
+            INPUT_DIR,
+            filename[:-4]
+        )
 
     pl = pv.Plotter(off_screen=True)
     pl.set_background("white")
     pl.add_mesh(
-        mesh,
-        scalars=curvature,
-        cmap="jet",
-        clim=[-1, 1],
+        geometry,
         show_scalar_bar=False,
         smooth_shading=True
     )
 
     for i in range(36):
-        mesh.rotate_z(10, inplace=True)
+        geometry.rotate_z(10, inplace=True)
+        pl.show(auto_close=False)
+        image = Image.fromarray(pl.image[:, 128:-128, :])
+        image.save(image_path + "_{:03d}.jpg".format(i))
+
+    pl.close()
+
+    cfd = pv.read(cfd_path)
+
+    if filename in train_geometries:
+        image_path = os.path.join(
+            DATASET_PATH,
+            IMAGE_DIR,
+            TRAIN_DIR,
+            TARGET_DIR,
+            filename[:-4]
+        )
+    else:
+        image_path = os.path.join(
+            DATASET_PATH,
+            IMAGE_DIR,
+            TEST_DIR,
+            TARGET_DIR,
+            filename[:-4]
+        )
+
+    pl = pv.Plotter(off_screen=True)
+    pl.set_background("gray")
+    pl.add_mesh(
+        cfd,
+        cmap="jet",
+        show_scalar_bar=False,
+        smooth_shading=True
+    )
+
+    for i in range(36):
+        cfd.rotate_z(10, inplace=True)
         pl.show(auto_close=False)
         image = Image.fromarray(pl.image[:, 128:-128, :])
         image.save(image_path + "_{:03d}.jpg".format(i))
@@ -55,3 +118,50 @@ for geometry in tqdm(geometries, desc="Processing ... "):
     pl.close()
 
     # break
+
+
+
+
+# -------------------------- v1 -------------------------------
+
+# for geometry in tqdm(geometries, desc="Processing ... "):
+#     geometry_path = os.path.join(
+#         DATASET_PATH,
+#         INPUT_DIR,
+#         geometry
+#     )
+
+#     image_path = os.path.join(
+#         DATASET_PATH,
+#         IMAGE_DIR,
+#         CURVATURE_DIR,
+#         geometry[:-4]
+#     )
+
+#     mesh = pv.read(geometry_path)
+#     # ecap = mesh.active_scalars
+#     # ecap = (ecap - np.mean(ecap)) / (np.std(ecap))
+#     curvature = mesh.curvature(curv_type="mean")
+#     curvature = (curvature - np.mean(curvature)) \
+#         / (np.std(curvature))
+
+#     pl = pv.Plotter(off_screen=True)
+#     pl.set_background("white")
+#     pl.add_mesh(
+#         mesh,
+#         scalars=curvature,
+#         cmap="jet",
+#         clim=[-1, 1],
+#         show_scalar_bar=False,
+#         smooth_shading=True
+#     )
+
+#     for i in range(36):
+#         mesh.rotate_z(10, inplace=True)
+#         pl.show(auto_close=False)
+#         image = Image.fromarray(pl.image[:, 128:-128, :])
+#         image.save(image_path + "_{:03d}.jpg".format(i))
+
+#     pl.close()
+
+#     # break
